76b979794921aecb23a478463f87336dd537dc63
BennyFeng <326489067@qq.com>
2023-03-08 19:02:10 +0800

    Remove comments

--- components/common/ProtocolToken.tsx
+++ components/common/ProtocolToken.tsx
@@ -46,57 +46,6 @@ const ProtocolToken: FC<Props> = ({ protocol }: Props) => {
             </div>
             <div className="card-panel-container">
               {getTableByDetailType(groupedProtocols[name], groupedProtocols[name][0].detail_types)}
-              {/*<Table header={[
-                { title: 'Token' },
-                { title: 'Price' },
-                { title: 'Amount' },
-                { title: 'USD Value' }
-              ]}>
-                {
-                  groupedProtocols[name].map((item: any) => {
-                    const supply_token_list = item?.supply_token_list ?? [];
-                    return (
-                      <div className="table-content-row border-b" key={`${protocol.chain}_${item.name}`}>
-                        <div className="flex-table-col">
-                    <div className="token-icons">
-                      <img src="https://static.debank.com/image/eth_token/logo_url/0x0c10bf8fcb7bf5412187a595ab97a3609160b5c6/70890e6172f62f4430bfeaff32680884.png" alt=""/>
-                      <img src="https://static.debank.com/image/eth_token/logo_url/0x6b175474e89094c44da98b954eedeac495271d0f/549c4205dbb199f1b8b03af783f35e71.png" alt=""/>
-                      <img src="https://static.debank.com/image/coin/logo_url/usdc/e87790bfe0b3f2ea855dc29069b38818.png" alt=""/>
-                      <img src="https://static.debank.com/image/coin/logo_url/usdt/23af7472292cb41dc39b3f1146ead0fe.png" alt=""/>
-                    </div>
-                    <div>
-                      USDD + BUSD
-                    </div>
-                  </div>*
-                        <div className="flex-table-col">
-                          <div className="token-icons">
-                            <div className="protocol-logo-wrap">
-                              <img className="token-icon" src={token.logo_url} alt=""/>
-                              {
-                                token.chain !== 'eth' && (
-                                  <img className="protocol-chain" src={chainLogoMap.get(token.chain)} alt={token.chain}/>
-                                )
-                              }
-                            </div>
-                          </div>
-                          <div>
-                            {token.optimized_symbol}
-                          </div>
-                        </div>
-                        <div>
-                          {formatDollar(token.price.toFixed(4), 4)}
-                        </div>
-                        <div>
-                          {token.amount.toFixed(4)}
-                        </div>
-                        <div>
-                          {formatDollar((token.price * token.amount).toFixed(2), 2)}
-                        </div>
-                      </div>
-                    )
-                  })
-                }
-              </Table>*/}
             </div>
           </div>
         ))
